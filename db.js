eac634a9ebe65cde94a1c81e70efe55baf672197
dentheroblox <eduverso4@gmail.com>
2025-12-18 11:26:53 +0800

    Fix MySQL Railway connection

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 // db.js
 import mysql from "mysql2/promise";
 
-export const db = await mysql.createPool({
+export const db = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
+  port: Number(process.env.MYSQLPORT) || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
